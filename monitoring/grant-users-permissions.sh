d2fd248ca6bea5a639296c513af4d8e20c3f7f63
Hian Silva <hiansilacts@gmail.com>
2025-08-01 16:51:23 -0300

    feat: adds db monitoring using prometheus+grafana

---
+++ monitoring/grant-users-permissions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Este script concede as permissões de monitoramento necessárias
+# para os usuários existentes de cada banco de dados.
+
+containers=("db_teams" "db_requests" "db_auth" "db_calendar" "db_competitions" "db_match_comments" "db_audit")
+users=("postgres" "IntegradorEstelar_42" "auth_user" "IntegradorMundial_87" "IntegradorGalatico_77" "IntegradorInternacional_94" "audit_user")
+databases=("db_teams" "db_requests" "auth_db" "db_calendar" "db_competitions" "db_match_comments" "audit_db")
+
+echo "Iniciando a concessão de permissões para monitoramento..."
+
+for i in "${!containers[@]}"; do
+  container="${containers[$i]}"
+  user="${users[$i]}"
+  database="${databases[$i]}"
+
+  if [[ "$user" == *"_"* ]]; then
+    sql_user="\"$user\""
+  else
+    sql_user="$user"
+  fi
+
+  # --- AJUSTE FINO NO COMANDO SQL ---
+  # Adicionamos 'pg_catalog.' para sermos mais específicos na permissão.
+  sql_command="GRANT pg_monitor TO $sql_user; GRANT EXECUTE ON FUNCTION pg_catalog.pg_postmaster_start_time() TO $sql_user; GRANT CONNECT ON DATABASE $database TO $sql_user;"
+
+  echo "-----------------------------------------------------"
+  echo "Aplicando permissões no contêiner: $container"
+  echo "Usuário: $user | Banco de Dados: $database"
+  
+  docker exec "$container" psql -U "$user" -d "$database" -c "$sql_command"
+  
+  if [ $? -eq 0 ]; then
+    echo "Permissões aplicadas com sucesso."
+  else
+    echo "ERRO ao aplicar permissões no contêiner $container."
+  fi
+done
+
+echo "-----------------------------------------------------"
+echo "Processo finalizado."
