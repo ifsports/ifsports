16ebedb56efebfe99b42a0c02ce2f941b160bcd7
Hian Silva <hiansilacts@gmail.com>
2025-08-02 02:36:20 -0300

    chore: reorganizate deploy.sh to work proper

--- deploy.sh
+++ deploy.sh
@@ -8,55 +8,51 @@ git checkout master
 git pull origin master
 
 echo "Atualizando o frontend"
-cd frontend-ifsports
+cd ~/ifsports/frontend-ifsports
 git pull origin main
-cd ..
 
 echo "Atualizando microserviços..."
 
 echo "Atualizando o auth-service..."
-cd ~/ifsports/auth_service_back/   
+cd ~/ifsports/auth_service_back
 git pull origin master
-cd ..
 
 echo "Atualizando o audit-service..."
-cd audit_service
+cd ~/ifsports/audit_service
 git pull origin master
-cd ..
 
 echo "Atualizando o calendar-service..."
-cd calendar-service
+cd ~/ifsports/calendar-service
 git pull origin main
-cd ..
 
 echo "Atualizando o competitions-service..."
-cd competitions-service
+cd ~/ifsports/competitions-service
 git pull origin main
-cd ..
 
 echo "Atualizando o match-comments-service..."
-cd match-comments-service
+cd ~/ifsports/match-comments-service
 git pull origin master
-cd ..
 
 echo "Atualizando o requests-service..."
-cd requests_service
+cd ~/ifsports/requests_service
 git pull origin master
-cd ..
 
 echo "Atualizando o teams-service..."
-cd teams_service
+cd ~/ifsports/teams_service
 git pull origin master
-cd ..
+
+cd ~/ifsports
 
 echo "Subindo os contêineres com Docker Compose..."
-docker-compose up --build -d --remove-orphans
 
-echo "Subindo os containers de Monitoramento do Banco..."
-docker-compose -f monitoring/docker-compose.monitoring.yml up --build -d --remove-orphans
+echo "Subindo Microserviços..."
+docker-compose up -d --build
 
-echo "Subindo os containers de Backup do Banco..."
-docker-compose -f docker-compose.backup.yml up --build -d --remove-orphans
+echo "Subindo Serviços de Backup..."
+docker-compose -f docker-compose.backup.yml up -d --build
+
+echo "Subindo Serviços de Monitoramento..."
+docker-compose -f monitoring/docker-compose.monitoring.yml up -d --build
 
 echo "Limpando imagens Docker antigas..."
 docker image prune -af
