a717e482605986f4d11b6bcb31e1814e8088eea5
Hian Silva <hiansilacts@gmail.com>
2025-08-02 00:24:23 -0300

    feat: adds databeses backup using GCS

--- monitoring/grant-users-permissions.sh
+++ monitoring/grant-users-permissions.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Este script concede as permissões de monitoramento necessárias
-# para os usuários existentes de cada banco de dados.
+# Este script concede as permissões necessárias para monitoramento e backup.
 
 containers=("db_teams" "db_requests" "db_auth" "db_calendar" "db_competitions" "db_match_comments" "db_audit")
 users=("postgres" "IntegradorEstelar_42" "auth_user" "IntegradorMundial_87" "IntegradorGalatico_77" "IntegradorInternacional_94" "audit_user")
 databases=("db_teams" "db_requests" "auth_db" "db_calendar" "db_competitions" "db_match_comments" "audit_db")
 
-echo "Iniciando a concessão de permissões para monitoramento..."
+echo "Iniciando a concessão de permissões..."
 
 for i in "${!containers[@]}"; do
   container="${containers[$i]}"
@@ -20,9 +19,14 @@ for i in "${!containers[@]}"; do
     sql_user="$user"
   fi
 
-  # --- AJUSTE FINO NO COMANDO SQL ---
-  # Adicionamos 'pg_catalog.' para sermos mais específicos na permissão.
-  sql_command="GRANT pg_monitor TO $sql_user; GRANT EXECUTE ON FUNCTION pg_catalog.pg_postmaster_start_time() TO $sql_user; GRANT CONNECT ON DATABASE $database TO $sql_user;"
+  # --- Comando SQL Atualizado com permissão de SELECT ---
+  sql_command="
+    GRANT pg_monitor TO $sql_user; 
+    GRANT EXECUTE ON FUNCTION pg_catalog.pg_postmaster_start_time() TO $sql_user; 
+    GRANT CONNECT ON DATABASE $database TO $sql_user;
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO $sql_user;
+  "
+  # Adicionamos 'GRANT SELECT' para permitir a leitura das tabelas para o backup.
 
   echo "-----------------------------------------------------"
   echo "Aplicando permissões no contêiner: $container"
